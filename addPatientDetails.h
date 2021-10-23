0ccddea4fba697484d0c7d82a1c00ffcb91adb60
ansh8tu <anshu8tu@gmail.com>
2021-10-23 22:40:23 +0530

    Commit-#7- Added patient disease as a data member to struct!

--- addPatientDetails.h
+++ addPatientDetails.h
@@ -5,6 +5,7 @@ typedef struct Patients{
     int pId;
     char pName[30];
     int pAge;
+    char pDiesase[30];
     int pCovidStatus;
 
 } Patient;
@@ -32,8 +33,8 @@ void addPatientDetails(){
     FILE* patientLog;
     FILE* tempLog;
 
-    patientLog = fopen("HospitalRecord.txt", "w+");
-    tempLog = fopen("TemporaryRecord.txt", "w+");
+    patientLog = fopen("HospitalRecord.txt", "a");
+    tempLog = fopen("TemporaryRecord.txt", "a");
 
     int previousEntry = autoPId - 1000; 
     int i;
@@ -49,11 +50,21 @@ void addPatientDetails(){
         printf("\nEnter the patient's age: ");
         scanf("%d", &pat[i].pAge);
 
+        printf("\nEnter the patient's disease: ");
+        scanf("%s", pat[i].pDiesase);
+
         printf("\nEnter the patient's covid status(1-Positive 0-Negative): ");
         scanf("%d", &pat[i].pCovidStatus);
 
         fwrite(&pat[i], sizeof(Patient), 1, patientLog);
         fwrite(&pat[i], sizeof(Patient), 1, tempLog);
+
+        printf("\nHello %s, your Patient-ID is: %d ", pat[i].pName, pat[i].pId);
+
+        if((strcmp(pat[i].pDiesase, "Headache") == 0)  || (strcmp(pat[i].pDiesase, "headache") == 0)){
+            printf("\nPlease refer to %s specialist in Ward 101", pat[i].pDiesase);
+        }
+        
     }
 
     FILE* writeAutoPId;
